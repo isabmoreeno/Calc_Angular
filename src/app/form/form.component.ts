ddba42057213388a6a5162e94f15c5b833f99a65
Isabela Moreno <isabeamoreno121@gmail.com>
2025-03-06 19:48:16 -0300

    first commit

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -10,12 +10,36 @@ export class FormComponent {
 
     num1: number = 0;
     num2: number = 0;
-    sum : number = 0;
+    resul : number = 0;
 
 
-   onClickSum(){
-      this.sum = this.num1 + this.num2;
+   somar(){
+      this.resul = this.num1 + this.num2;
    } 
+
+    subtrair(){
+      this.resul = this.num1 - this.num2;
+   }
+
+   dividir(){
+      this.resul = this.num1 / this.num2;
+   }
+
+   multiplicar(){
+      this.resul = this.num1 * this.num2;
+   }
+
+   potencia() {
+      this.resul = Math.pow(this.num1, this.num2);
+    }
+  
+    raiz() {
+      if (this.num1 >= 0) {
+        this.resul = Math.sqrt(this.num1);
+      } else {
+        alert("Não é possível calcular a raiz quadrada desse número!");
+      }
+    }
 
 
 
